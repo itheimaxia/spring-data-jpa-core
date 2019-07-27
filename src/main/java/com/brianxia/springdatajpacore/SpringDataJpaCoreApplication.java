851df17037860b2d231aa76dedb0559860a62847
itheimaxia <xiadong@itcast.cn>
2019-07-27 23:10:03 +0800

    验证懒加载模式
    验证懒加载模式

--- src/main/java/com/brianxia/springdatajpacore/SpringDataJpaCoreApplication.java
+++ src/main/java/com/brianxia/springdatajpacore/SpringDataJpaCoreApplication.java
@@ -1,13 +1,17 @@
 package com.brianxia.springdatajpacore;
 
+import com.brianxia.springdatajpacore.utils.SpringBootApplicationContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringDataJpaCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataJpaCoreApplication.class, args);
+
+        ConfigurableApplicationContext run = SpringApplication.run(SpringDataJpaCoreApplication.class, args);
+        SpringBootApplicationContextUtil.setApplicationContext(run);
     }
 
 }
--- src/main/java/com/brianxia/springdatajpacore/contoller/DemoController.java
+++ src/main/java/com/brianxia/springdatajpacore/contoller/DemoController.java
@@ -1,18 +1,23 @@
 package com.brianxia.springdatajpacore.contoller;
 
 import com.brianxia.springdatajpacore.dao.CustomerDao;
+import com.brianxia.springdatajpacore.utils.SpringBootApplicationContextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 由于需要懒加载测试，通过ApplicationContext获取到接口实现类。
+ */
 @RestController
 public class DemoController {
 
-    @Autowired
-    private CustomerDao customerDao;
+//    @Autowired
+//    private CustomerDao customerDao;
 
     @GetMapping(value = "test")
     public String getResult(){
+        CustomerDao customerDao = SpringBootApplicationContextUtil.getBean(CustomerDao.class);
         return customerDao.findOne(0L).getCustName();
     }
 }
